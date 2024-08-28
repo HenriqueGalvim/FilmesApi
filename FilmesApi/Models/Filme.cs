ab825b04c7964f4fff21dc0c4f337bd777310df0
henrixe <henrixe80@gmail.com>
2024-08-28 11:46:19 -0400

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    public Filme(string titulo, string genero, int duracao)
+    {
+        Titulo = titulo;
+        Genero = genero;
+        Duracao = duracao;
+    }
+
+    [Required(ErrorMessage ="O titulo do filme é obrigatória")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    public int Duracao { get; set; }
+
+    public void ExibirDados()
+    {
+        Console.WriteLine("Titulo: ",this.Titulo);
+        Console.WriteLine("Genero: ",this.Genero);
+        Console.WriteLine("Duracao: ",this.Duracao);
+    }
+}
